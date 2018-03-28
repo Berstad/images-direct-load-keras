e952a8b370b6bac4915bd08fbfdb8b2892c6e5a3
Berstad <tor.berstad@gmail.com>
2018-03-28 17:27:05 +0200

    Changed to work with TFmetrics

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+This repository is a fork of:
+https://github.com/kelkalot/images-direct-load-keras
+
+It includes changes which make it function with the repository:
+https://github.com/Berstad/TFmetrics/ (This project may be private at the time of reading)
+
 # images-direct-load-keras
 This repo provides code that shows how images can be directly loaded for being useable in Keras (or others) 
 without using the generator function. This can be useful for some applications.
---
+++ loadimg.py.py
@@ -0,0 +1,65 @@
+import os.path
+import os
+import cv2
+import numpy as np
+
+
+# Main script containing the image loading part. In keras-nn-example.py an example of a CNN using images loaded in that way is given.
+
+# Root directory containing the images to classify, folder are used as labels
+# train --> cat --> img1, img2...imgN
+#       --> dog --> img1, img2...imgN
+#
+# test  --> cat --> img1, img2...imgN
+#       --> dog --> img1, img2...imgN
+
+
+def getimagedataandlabels(root_dir, image_w, image_h, verbose, rescale, mode):
+
+    X_data=[]
+    Y_data=[]
+    classes_from_directories = []  # to determine the classes from the root folder structure automatically
+
+    for directory, subdirectories, files in os.walk(root_dir):
+        if verbose:
+            print(directory)
+        sorted(subdirectories)
+        for subdirectory in subdirectories:
+            if verbose:
+                print(subdirectory)
+            classes_from_directories.append(subdirectory)
+        for file in files:
+            # print(file)
+            # print(directory)
+            if file != '.DS_Store':  # fix for MAC...
+                imagepath = os.path.join(directory, file)
+                current_image_class_splitt = imagepath.split('/')
+                current_image_class = current_image_class_splitt[len(current_image_class_splitt) - 2]
+                img = cv2.imread (imagepath)
+                img = cv2.resize(img, (image_w, image_h))
+                if mode == "custom_seq":
+                    img_tensor = np.expand_dims(img_tensor, axis=0)  # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
+                np.true_divide(img,rescale)  # imshow expects values in the range [0, 1]
+                X_data.append(np.asarray(img, dtype="int32"))
+                Y_data.append(current_image_class)
+                #print imagepath
+
+    return np.array(X_data), np.array(Y_data), classes_from_directories
+
+if __name__ == '__main__':
+	#sub folders in the root directory respresen classes, for example '/Users/michael/polyps' can contain a /neg/images... and pos/images...
+	root_dir_train = 'INSERT PATH HERE'
+	root_dir_test = 'INSERT PATH HERE'
+
+	#Reshaping the images durring the loading process
+	image_w, image_h = 256,256
+
+	x_train, y_train = getimagedataandlabels(root_dir_train,image_w,image_h)
+	print "Training data and labels loaded"
+	print x_train.shape
+	print y_train.shape
+
+	print "Test data and labels loaded"
+	x_test, y_test = getimagedataandlabels(root_dir_test,image_w,image_h)
+	print x_test.shape
+	print y_test.shape
